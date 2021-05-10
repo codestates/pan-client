1f837bc86bdcd8bfa04d73fc8ffd1d8676a20f86
lunaticholic <lunaticholic@gmail.com>
2021-05-10 12:24:10 +0400

    1.2 대충 로그인화면

---
+++ src/components/FontAction.js
@@ -0,0 +1,13 @@
+import Styled from "styled-components";
+
+export const BlueFont = Styled.text`
+    font-size: 24px;
+    font-weight: 700;
+    color: rgb(61, 141, 171);
+`;
+
+export const RedFont = Styled.text`
+    font-size: 24px;
+    font-weight: 700;
+    color: rgb(216, 89, 65);
+`;
