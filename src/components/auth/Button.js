ef9e6f085eee120dfd45430a842878e95816b275
이정민 <leejungmin@MacBook-Pro.local>
2021-05-18 19:34:02 +0900

    initial operation

--- src/components/auth/Button.js
+++ src/components/auth/Button.js
@@ -3,14 +3,22 @@ import styled from "styled-components";
 const Button = styled.input`
     border: none;
     margin-top: 12px;
-    background-color: ${props => props.theme.accent};
+    background-color:  #005c99;
+    /* background-color: ${props => props.theme.accent}; */
     border: 1px solid;
-    color: black;
+    border-radius: 10px;
+    cursor: pointer;
+    color: #ffffff;
     text-align: center;
     padding: 8px 0px;
     font-weight: 600;
     width: 80%;
     /* opacity: ${(props) => props.disabled ? "0.2" : "1"} */
+    &:hover {
+        transform: scale(1.03);
+        color: gold;
+        border: none;
+    }
 `;
 
 export default Button;
