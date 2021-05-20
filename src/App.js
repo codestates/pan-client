114810ff781ac7f646bb6e9f5ed6b6e10ee8f562
lunaticholic <lunaticholic@gmail.com>
2021-05-20 19:38:37 +0400

    1.0 배경색상 정하기

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import routes from "./routes";
 import Main from "./screens/Main";
-import Randing from "./screens/Randing";
+import Landing from "./screens/Landing";
 import Login from "./screens/Login";
 import SignUp from "./screens/SignUp";
 import { GlobalStyles } from "./styles";
@@ -14,8 +14,8 @@ function App() {
         <GlobalStyles />
         <Router>
           <Switch>
-            <Route path={routes.randing} exact>
-              <Randing />
+            <Route path={routes.landing} exact>
+              <Landing />
             </Route>
             <Route path={routes.main} >
               <Main />
