e4ebf8a72dc9cfbc9868829e848581f19c3eb5d6
lunaticholic <lunaticholic@gmail.com>
2021-05-23 21:28:44 +0400

    젠장맞을 code

--- src/components/auth/FontLayout.js
+++ src/components/auth/FontLayout.js
@@ -4,121 +4,121 @@ export const TextAlign = styled.div`
     text-align: center;
 `;
 
-export const BlueGreen = styled.text`
+export const BlueGreen = styled.label`
     font-size: 24px;
     font-weight: 700;
     color: rgb(61, 141, 171);
 `;
 
-export const CedarChest = styled.text`
+export const CedarChest = styled.label`
     font-size: 24px;
     font-weight: 700;
     color: rgb(216, 89, 65);
 `;
 
-export const ArylideYellow = styled.text`
+export const ArylideYellow = styled.label`
     font-size: 24px;
     font-weight: 700;
     color: #E2CD6D;
 `;
 
-export const DutchWhite = styled.text`
+export const DutchWhite = styled.label`
     font-size: 24px;
     font-weight: 700;
     color: #E4D8B4;
 `;
 
-export const DarkSeaGreen = styled.text`
+export const DarkSeaGreen = styled.label`
     font-size: 24px;
     font-weight: 700;
     color: #83B799;
 `;
 
-export const CadetBlue = styled.text`
+export const CadetBlue = styled.label`
     font-size: 24px;
     font-weight: 700;
     color: #75A5A9;
 `;
 
-export const Alabaster = styled.text`
+export const Alabaster = styled.label`
     font-size: 24px;
     font-weight: 700;
     color: #E3DFD4;
 `;
 
-export const BrownSugar = styled.text`
+export const BrownSugar = styled.label`
     font-size: 24px;
     font-weight: 700;
     color: #C57951;
 `;
 
-export const EerieBlack = styled.text`
+export const EerieBlack = styled.label`
     font-size: 24px;
     font-weight: 700;
     color: #1C1B1A;
 `;
 
-export const DarkSlateGray = styled.text`
+export const DarkSlateGray = styled.label`
     font-size: 24px;
     font-weight: 700;
     color: #274040;
 `;
 
-export const DarkLiverHorses = styled.text`
+export const DarkLiverHorses = styled.label`
     font-size: 24px;
     font-weight: 700;
     color: #553F3A;
 `;
 
-export const InOrangeGGB = styled.text`
+export const InOrangeGGB = styled.label`
     font-size: 24px;
     font-weight: 700;
     color: #BB2B2A;
 `;
 
-export const OldLace = styled.text`
+export const OldLace = styled.label`
     font-size: 24px;
     font-weight: 700;
     color: #FFF9E9;
 `;
 
-export const ColumbiaBlue = styled.text`
+export const ColumbiaBlue = styled.label`
     font-size: 24px;
     font-weight: 700;
     color: #CCDEE2;
 `;
 
-export const Rajah = styled.text`
+export const Rajah = styled.label`
     font-size: 24px;
     font-weight: 700;
     color: #F4A24F;
 `;
 
-export const Iris = styled.text`
+export const Iris = styled.label`
     font-size: 24px;
     font-weight: 700;
     color: #505AC5;
 `;
 
-export const Glaucous = styled.text`
+export const Glaucous = styled.label`
     font-size: 24px;
     font-weight: 700;
     color: #7886E1;
 `;
 
-export const MaxBluePurple = styled.text`
+export const MaxBluePurple = styled.label`
     font-size: 24px;
     font-weight: 700;
     color: #9196E1;
 `;
 
-export const PastelPink = styled.text`
+export const PastelPink = styled.label`
     font-size: 24px;
     font-weight: 700;
     color: #DCB1B7;
 `;
 
-export const RosyBrown = styled.text`
+export const RosyBrown = styled.label`
     font-size: 24px;
     font-weight: 700;
     color: #C08A90;
